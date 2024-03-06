1dace48f7ac92518fabce4564e61719260a0672f
Valéria Carneiro <valerialribeiroc@gmail.com>
2024-03-06 18:20:16 -0300

    Remove comentários

--- server.rb
+++ server.rb
@@ -3,9 +3,7 @@ require 'rack/handler/puma'
 require 'pg'
 require 'json'
 
-# Método para consultar o banco de dados e retornar os resultados em JSON
 def get_data_from_database
-  # Configurações do banco de dados PostgreSQL
   db_config = {
     dbname: 'postgres',
     user: 'rebase',
@@ -17,12 +15,10 @@ def get_data_from_database
   result = conn.exec('SELECT * FROM exams')
   conn.close
 
-  # Converter os resultados em um formato JSON
   data = result.map { |row| row.to_h }
   data.to_json
 end
 
-# Endpoint para obter dados do banco de dados e retorná-los como JSON
 get '/tests' do
   content_type :json
   get_data_from_database
